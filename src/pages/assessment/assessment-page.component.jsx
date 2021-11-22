50dc444b9280e9b7739a095fae1cf89e3b0aa773
NoSword <alexjonesc@gmail.com>
2021-11-22 11:21:36 -0800

    Refactored assesment component to use hooks

--- src/pages/assessment/assessment-page.component.jsx
+++ src/pages/assessment/assessment-page.component.jsx
@@ -1,54 +1,51 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { initAssessment } from '../../redux/assessment/assessment.actions'
 import {
-  selectAssessmentTitle,
+  selectHasAssessment,
   selectAssessmentId,
+  selectAssessmentTitle,
 } from '../../redux/assessment/assessment.selectors'
 import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
 import './assessment-page.styles.scss'
 
 const AssessmentView = (props) => {
-  const { id, title } = props
+  const { assessmentId, title } = props
   return (
     <div className="assessment-page">
       <div className="assessment-page__content">
         <div>
-          ID: {id}, Assessment Title: {title}
+          assessmentId: {assessmentId}, Assessment Title: {title}
         </div>
       </div>
     </div>
   )
 }
 
-const AssessmentVIewWithSpinner = WithSpinner(AssessmentView)
+const AssessmentViewWithSpinner = WithSpinner(AssessmentView)
 
-const Assessment = ({ isLoading, initAssessment, ...otherPros }) => {
+const Assessment = () => {
+  const dispatch = useDispatch()
   const { id } = useParams()
-  //const dispatch = useDispatch()
+  const isLoading = useSelector(selectHasAssessment)
+
+  const assessmentData = {
+    assessmentId: useSelector(selectAssessmentId),
+    title: useSelector(selectAssessmentTitle),
+  }
 
   // initialize assessment
   useEffect(() => {
-    initAssessment(id)
-  }, [initAssessment, id])
+    dispatch(initAssessment(id))
+  }, [dispatch, id])
 
   return (
     <div>
-      <AssessmentVIewWithSpinner isLoading={isLoading} {...otherPros} />
+      <AssessmentViewWithSpinner isLoading={isLoading} {...assessmentData} />
     </div>
   )
 }
 
-const mapStateToProps = (state) => ({
-  id: selectAssessmentId(state),
-  title: selectAssessmentTitle(state),
-  isLoading: !state.assessment.assessment,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  setSelectedFilters: (id) => dispatch(initAssessment(id)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Assessment)
+export default Assessment
--- src/redux/assessment/assessment.selectors.js
+++ src/redux/assessment/assessment.selectors.js
@@ -2,6 +2,11 @@ import { createSelector } from 'reselect'
 
 const selectAssessmentState = (state) => state.assessment
 
+export const selectHasAssessment = createSelector([selectAssessmentState], (assessmentState) => {
+  console.log(!assessmentState.assessment, assessmentState.assessment)
+  return !assessmentState.assessment
+})
+
 const selectAssessment = createSelector([selectAssessmentState], (assessmentState) => {
   return assessmentState.assessment ? assessmentState.assessment : {}
 })
