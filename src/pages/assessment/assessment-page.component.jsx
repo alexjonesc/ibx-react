91392fa9fd57308a4f626c59e0263740eb78bd09
NoSword <alexjonesc@gmail.com>
2021-11-23 15:14:54 -0800

    small cleanup

--- src/pages/assessment/assessment-page.component.jsx
+++ src/pages/assessment/assessment-page.component.jsx
@@ -19,80 +19,6 @@ import './assessment-page.styles.scss'
 const AssessmentView = (props) => {
   const { assessmentId, title, questions } = props
 
-  const test = {
-    elements: {
-      'pp-pie-element-select-text': '@pie-element/select-text@6.5.8',
-    },
-    markup: '<pp-pie-element-select-text id="p-00000000"></pp-pie-element-select-text>',
-    id: 'p-00000000',
-    models: [
-      {
-        maxSelections: 2,
-        rationaleEnabled: false,
-        prompt: '<div>Select the VERBS in the following sentence.</div>',
-        teacherInstructionsEnabled: false,
-        text: 'Please practice using the New Item Bank.',
-        element: 'pp-pie-element-select-text',
-        feedbackEnabled: false,
-        partialScoring: true,
-        mode: '',
-        id: 'p-00000000',
-        scoringType: 'auto',
-        promptEnabled: true,
-        tokens: [
-          {
-            text: 'practice',
-            end: 15,
-            start: 7,
-            correct: true,
-          },
-          {
-            start: 16,
-            end: 21,
-            correct: true,
-            text: 'using',
-          },
-          {
-            text: 'the',
-            end: 25,
-            start: 22,
-          },
-          {
-            text: 'New',
-            start: 26,
-            end: 29,
-          },
-          {
-            end: 34,
-            start: 30,
-            text: 'Item',
-          },
-          {
-            text: 'Bank',
-            end: 39,
-            start: 35,
-          },
-        ],
-        feedback: {
-          correct: {
-            type: 'default',
-            default: 'Correct',
-          },
-          partial: {
-            type: 'default',
-            default: 'Nearly',
-          },
-          incorrect: {
-            type: 'default',
-            default: 'Incorrect',
-          },
-        },
-        highlightChoices: true,
-        studentInstructionsEnabled: false,
-      },
-    ],
-  }
-
   const onPlayerError = (e) => {
     console.log('XXXXXXXXX ========>', e)
   }
@@ -115,7 +41,6 @@ const AssessmentView = (props) => {
                 <div>
                   <pie-player
                     id={`player-${item.itemId}`}
-                    config={test}
                     bundleHost="prod"
                     load-complete={onPlayerError}
                     player-error={onPlayerError}
